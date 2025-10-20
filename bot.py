53e8ebdc8a5873ee6ada457fe7227859c8da3177
hutorskoi13-tech <hutorskoi13@gmail.com>
2025-10-20 22:14:11 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -169,3 +169,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+from flask import Flask
+import threading
+
+def run_web():
+    app = Flask("")
+    @app.route('/')
+    def home():
+        return "Bot is running"
+    app.run(host='0.0.0.0', port=8080)
+
+threading.Thread(target=run_web).start()
